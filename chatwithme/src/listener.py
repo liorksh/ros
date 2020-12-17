6c7238cbeaa8c1fe44a15e9422a54177b8441b9b
Lior K Sh <30384199+liorksh@users.noreply.github.com>
2020-12-18 07:27:11 +0800

    Add comments

--- chatwithme/src/listener.py
+++ chatwithme/src/listener.py
@@ -14,9 +14,8 @@ def listener():
         
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
+    # anonymous=True flag means that rospy will choose a unique name for this node.
+    # With that, multiple listeners can run simultaneously.
     rospy.init_node('my_listener', anonymous=True)
 
     rospy.loginfo('starting the listener node, enter "q" to exit')
