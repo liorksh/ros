b7242b6e98f3b55e20dffa1bb217bfad83188c51
Lior K Sh <30384199+liorksh@users.noreply.github.com>
2020-12-18 07:31:22 +0800

    Add comments

--- chatwithme/src/talker.py
+++ chatwithme/src/talker.py
@@ -10,6 +10,9 @@ def talker():
     # the topic message is String (not the Python string, but the std_msg String)
     # the queue size is the number of messages that will be retained before starting to delete (in case there are no subscribers).
     pub = rospy.Publisher('chatwithme', String, queue_size=5)
+
+    # the name of the node is dynamic to avoid multiple nodes with the same name.
+    # With that, multiple listeners can run simultaneously.
     rospy.init_node('my_talker', anonymous=True)
 
     rospy.loginfo('starting the talker node, enter "q" to exit')
@@ -25,6 +28,7 @@ def talker():
 
 if __name__ == '__main__':
     try:
+        # setting a thread to listen to the user's input, so that the process is stopped gracefully.
         threading.Thread(target=utils.waitForInput).start()
         talker()
     except rospy.ROSInterruptException:
